e84e59e689d9f46e46d6c0a424e080ab61d11dbc
unknown <taktuk07@gmail.com>
2026-02-12 11:45:13 +0200

    finish build the api and the qury

--- analytics-api/app/connection.py
+++ analytics-api/app/connection.py
@@ -1,1 +1,16 @@
-fastapi
+import mysql.connector
+import os
+
+
+class MysqlManager:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(
+            host=os.getenv('MYSQL_HOST','mysql'),
+            user=os.getenv('MYSQL_USER','appuser'),
+            password=os.getenv('MYSQL_PASSWORD','apppass'),
+            database=os.getenv('MYSQL_DATABASE','users'),
+        )
+        except mysql.connector.Error as err:
+            raise err
+        self.cruser = self.connection.cursor()
--- analytics-api/app/routes.py
+++ analytics-api/app/routes.py
@@ -1,10 +1,22 @@
 
 from fastapi import FastAPI, HTTPException, APIRouter
-
-
+import  qury
 app = FastAPI()
 router = APIRouter()
 router = FastAPI.routes()
+my_qury = qury.Mysql_Service()
 
-
-@router.get()
+@router.get('/analytics/top-customers')
+def get_top():
+    try:
+        return my_qury.get_top_10()
+    except HTTPException as e:
+        return str(e)
+@router.get('/analytics/customers-without-orders')
+def customers_without_orders():
+    try:
+        return my_qury.get_customers_without_orders()
+    except HTTPException as e:
+        return str(e)
+@router.get('/analytics/zero-credit-active-customers')
+def get_zero_credit_active_customers():
